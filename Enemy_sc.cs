2e89d62e884fe3c4f911e5f5a0bf867b0d2e690f
Muaz-RD <168585722+Muaz-RD@users.noreply.github.com>
2024-10-22 18:58:40 +0300

    Add files via upload

---
+++ Enemy_sc.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Enemy_sc : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 3.0f;
+
+    void Update()
+    {
+        transform.Translate(Vector3.down * speed * Time.deltaTime);
+
+        if (transform.position.y <= -5.2)
+        {
+            float randomx = Random.Range(-10f, 10f);
+            transform.position = new Vector3(randomx, 5.2f, 0);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        player_sc oyuncu = other.transform.GetComponent<player_sc>();
+        oyuncu.Demage();
+
+
+        if (other.tag == "player")  
+        {
+            Destroy(this.gameObject);
+        }
+        else if (other.tag == "Laser")
+        {
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+
+}
